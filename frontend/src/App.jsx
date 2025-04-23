02abdd34da6048d493a1e5b007d14921ea6a0f52
Shivam Darekar <shivamdarekar123@gmail.com>
2025-04-23 11:38:40 +0530

    Shivam - Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/customer-dashboard" element={<Dashboard />} />
             <Route path="/features" element={<FeaturesSectionDemo />} />
             {/* <Route path ="/contact" element={<ContactUs />} /> */}
           </Routes>
